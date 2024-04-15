f15a7ae60d2152f1af039769813561606f09180f
Patricia Alonso Ayllón <alonso.aypat22@triana.salesianos.edu>
2024-04-15 12:39:24 +0200

    fqhnl

---
+++ ejemploControlador/src/test/java/com/salesianos/dam/ejemplocontrolador/EjemploControladorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.dam.ejemplocontrolador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploControladorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
