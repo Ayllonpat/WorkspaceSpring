6542951efde8594638b9c456f4e78dc09fd4a539
Patricia Alonso Ayllón <alonso.aypat22@triana.salesianos.edu>
2024-04-23 09:05:28 +0200

    remplazar

---
+++ ejemploUsuariott/src/main/java/com/salesianos/dam/ejemplocontrolador2/EjemploUsuariottApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.dam.ejemplocontrolador2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploUsuariottApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploUsuariottApplication.class, args);
+	}
+
+}
---
+++ ejemploUsuariott/src/test/java/com/salesianos/dam/ejemplocontrolador2/EjemploUsuariottApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.dam.ejemplocontrolador2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploUsuariottApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
